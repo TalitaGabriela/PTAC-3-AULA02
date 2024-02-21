de5e64f3371b4d405a8b1cc3a0ff0cd1c4053420
TalitaGabriela <talitagabriela1216@gmail.com>
2024-02-21 14:09:54 -0400

    Commit
    aula 21/02

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,11 +1,16 @@
+import Link from "next/link"
 export default function Menu(){
     return(
         <header>
-            <h1>Menu</h1>
+            <h3>Menu</h3>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Registrar</li>
+                    <Link href="/">
+                        <li>Home</li>
+                    </Link>
+                    <Link href="/registro">
+                        <li>Registrar</li>
+                    </Link>
                 </ul>    
             </nav>
         </header>
